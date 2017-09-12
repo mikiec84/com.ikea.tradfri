ccdd4b1f6dccbd66562ea5b5d2f93ce95da39aca
robinbolscher@gmail.com <robinbolscher@gmail.com>
2017-09-12 16:35:26 +0200

    Test

--- drivers/motion_sensor/device.js
+++ drivers/motion_sensor/device.js
@@ -12,7 +12,7 @@ class MotionSensor extends ZigBeeDevice {
 		// 	.then(rsp => console.log(rsp))
 		// 	.catch(err => console.error(err))
 		//
-		// this.node.endpoints[0].clusters['genOnOff'].read('onOff')
+		// this.node.endpoints[0].clusters['genAlarms'].read('alarm')
 		// 	.then(rsp => console.log(rsp))
 		// 	.catch(err => console.error(err))
 
@@ -21,21 +21,38 @@ class MotionSensor extends ZigBeeDevice {
 		// 	console.log(await this.node.endpoints[0].clusters['genOnOff'].read('onOff'));
 		// 	console.log(await this.node.endpoints[0].clusters['genPowerCfg'].read('batteryVoltage'));
 		// }, 10000);
+		// this.log(this.node.endpoints[0].clusters['gebBasic'])
 
-		for (let cluster in this.node.endpoints[0].clusters) {
+		this.node.endpoints[0].clusters['genBasic'].report('65533', 10, 15, null, (err) => {
+			console.log('genAlarms', err);
+		});
 
-			if (cluster !== 'zapp') {
-				this.log('bind', cluster)
-				try {
-					this.registerReportListener(cluster, null, report => {
-						this.log(cluster, 'report');
-						this.log(report);
-					})
-				} catch (err) {
-					console.error(err)
-				}
-			}
-		}
+		// this.node.endpoints[0].clusters['genPowerCfg'].report('batteryPercentageRemaining', 10, 15, null, (err) => {
+		// 	console.log('genPowerCfg', err);
+		// });
+		//
+		// this.node.endpoints[0].clusters['genAlarms'].report('alarmCount', 10, 15, null, (err) => {
+		// 	console.log('genAlarms', err);
+		// });
+		// this.registerReportListener('genPowerCfg', 'batteryPercentageRemaining', report => {
+		// 	this.log('genPowerCfg', 'batteryPercentageRemaining');
+		// 	this.log(report);
+		// });
+		//
+		// for (let cluster in this.node.endpoints[0].clusters) {
+		//
+		// 	if (cluster !== 'zapp') {
+		// 		this.log('bind', this.node.endpoints[0].clusters[cluster].attrs)
+		// 		// try {
+		// 		// 	this.registerReportListener(cluster, null, report => {
+		// 		// 		this.log(cluster, 'report');
+		// 		// 		this.log(report);
+		// 		// 	})
+		// 		// } catch (err) {
+		// 		// 	console.error(err)
+		// 		// }
+		// 	}
+		// }
 	}
 }
 
